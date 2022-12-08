1ac14f0c74bb86597f6322f0b5474c3491d2d72f
Fernando Dingler <fdingler@amazon.com>
2022-12-08 14:28:36 -0800

    add new-app

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -10,7 +10,7 @@ export function Index() {
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome app 👋
+            Welcome to NextJS 👋
           </h1>
         </div>
 
